5de76de3765e08acbd9aa223158e2d10ae524297
Dan Farino <dan.farino@eyecuelab.com>
2021-11-06 20:40:39 -0700

    move docker image to GCP Artifact Registry

--- build-and-push.sh
+++ build-and-push.sh
@@ -5,5 +5,5 @@ set -Eeuo pipefail
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 cd "$SCRIPT_DIR" || exit 1
 
-docker build -t us.gcr.io/eyecue-io/eyecue-codemap:latest .
-docker push us.gcr.io/eyecue-io/eyecue-codemap:latest
+docker build -t us-central1-docker.pkg.dev/eyecue-ops/eyecue-codemap/eyecue-codemap .
+docker push us-central1-docker.pkg.dev/eyecue-ops/eyecue-codemap/eyecue-codemap:latest
