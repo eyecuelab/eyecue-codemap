e83fb7a5cc505f2c5d125e75744e21d038b60042
Dan Farino <dan.farino@eyecuelab.com>
2022-03-09 14:24:57 -0800

    fix broken version

--- deploy/update_go.sh
+++ deploy/update_go.sh
@@ -2,14 +2,17 @@
 
 # Note: this script is meant to be sourced vs. executed because it exports PATH
 
-version=1.17.6
+(
+    version=1.17.6
 
-echo "Installing Go $version ..."
+    echo "Installing Go $version ..."
 
-curl -sSLO https://golang.org/dl/go$version.linux-amd64.tar.gz
+    curl -sSLO https://golang.org/dl/go$version.linux-amd64.tar.gz
 
-sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf go$version.linux-amd64.tar.gz
+    sudo rm -rf /usr/local/go
+    sudo tar -C /usr/local -xzf go$version.linux-amd64.tar.gz
+
+)
 
 export PATH="/usr/local/go/bin:$PATH"
 go version
