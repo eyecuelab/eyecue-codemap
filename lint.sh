7c9dbb995aff6676c76256fb26a6d8a9fd033376
Dan Farino <dan.farino@eyecuelab.com>
2022-08-13 18:08:46 -0700

    fix golangci-lint version command

--- lint.sh
+++ lint.sh
@@ -3,5 +3,5 @@
 set -euo pipefail
 
 command -v golangci-lint >/dev/null 2>&1 || { echo 'Please install https://golangci-lint.run/'; exit 1; }
-golangci-lint --version
+golangci-lint version
 golangci-lint run --timeout 10m
